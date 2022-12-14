904b8b1dd40fec0f69773b4f6411acfea9d37d84
Jack Scarpelli <Festivetomcat99@gmail.com>
2022-12-14 18:57:34 -0500

    commit tree 648970f704985a4edc0126cf8d23066273a8305c

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+const currentUser = 'Jack S.';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+console.log(welcomeMessage);
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+console.log(excitedWelcomeMessage);
+
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+console.log(shortGreeting);
